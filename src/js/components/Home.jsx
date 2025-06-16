43c51785a37c9575a94d6d3af35d249be7317756
Jonathan <jonyoner@gmail.com>
2025-06-16 11:04:58 +0000

    planteamiento inicial

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [newTask, setNewTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+	const [taskCount, setTaskCount] = useState(0);
+	
+    function addTask(event) {
+		if (event.key === "Enter"){
+		let newArray = tasks.concat(newTask)	
+		}
+	}
+	
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		    <h1>Mis Tareas</h1>
+			<ul>
+			<li><input type="text" placeholder="¿Qué tengo que hacer?" onChange = { (event) => setNewTask(event.target.value)} onKeyDown={addTask}></input></li>
+			<li>Buenas tardes</li>
+			<li>Buenos dias</li>
+			<li>Buena noches</li>
+			</ul>
+
+		
 		</div>
 	);
 };
